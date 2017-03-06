290ff9c8f9693e7eb1a9be9cd9a14e2a4d3bdfce
Robert Kulp <pedalinon@gmail.com>
2017-03-06 15:47:39 -0800

    Angular nav adaptation

--- assets/angular/header.js
+++ assets/angular/header.js
@@ -8,4 +8,32 @@
 	indexPage.controller("navControl", function($scope) {
 
 	});
+/*-------------------------------------------------------------------*/
+/*  4. Page scrolling feature, requires jQuery Easing plugin.
+/*-------------------------------------------------------------------*/
+	indexPage.controller("scrollEase", function($scope) {
+			 var pageScroll = function(){
+        $('.page-scroll > a').bind('click', function(e){
+            e.preventDefault();
+            
+            var anchor = $(this),
+            href = anchor.attr('href'),
+            offset = $('body').attr('data-offset');
+            
+            $('html, body').stop().animate({
+                scrollTop: $(href).offset().top - (offset - 1)
+            }, 1500, 'easeInOutExpo');
+            
+            /*
+             * Automatically retract the navigation after clicking 
+             * on one of the menu items.
+             */
+            if(!$(this).parent().hasClass('dropdown')){
+                $('.berg-collapse').collapse('hide');
+            }
+        });
+    };
+    
+    pageScroll();
+	});
 }());
